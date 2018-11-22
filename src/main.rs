08483867edff106df9e6cd7c7b9d60e8928bf169
Nils Martel <nils.martel@concepts-system.com>
2018-11-22 19:09:48 +0100

    Complete Code

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{stdin, Read, Result};
+
+fn main() {
+    let sum = stdin_to_string()
+        .expect("failed to read from stdin")
+        .split('\n')
+        .map(|s| s.parse::<f64>())
+        .fold(0.0, |a, r| {
+            a + match r {
+                Ok(n) => n,
+                _ => 0.0,
+            }
+        });
+    println!("{}", sum);
+}
+
+fn stdin_to_string() -> Result<String> {
+    let mut buffer = String::new();
+    let stdin = stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
